3ee1793ee162cf706be0fd66e405d0f5b97426a7
alf1e724 <alfiechriswatson2021@gmail.com>
2025-10-30 17:05:20 +0000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -35,3 +35,15 @@ export function score(rank, percent, minPercent) {
 
     return Math.max(round(score), 0);
 }
+
+export function round(num) {
+    if (!('' + num).includes('e')) {
+        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+    } else {
+        var arr = ('' + num).split('e');
+        var sig = '';
+        if (+arr[1] + scale > 0) {
+            sig = '+'
+        }
+    }
+}
