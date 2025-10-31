46e75932e44a3dfa21f58b445075266d6613ac01
alf1e724 <alfiechriswatson2021@gmail.com>
2025-10-31 00:17:52 +0000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (350 * Math.pow(0.54, rank - 1));
+    let score = (350 * Math.pow(0.535, rank - 1));
 
     score = Math.max(0, score);
 
