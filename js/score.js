69df5e1444fc756e35a7648bdacb331283e14391
smegful dashers <xmollyxgood@gmail.com>
2026-08-02 20:03:50 +0100

    Adjust rank threshold in score function

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 139) {
+    if (rank > 138) {
         return 0;
     }
     if (rank > 75 && percent < 100) {
